3f6e43c8850ab7ccfd9d6dec3c53aaac6f756399
mahshuk410 <s10204884@connect.np.edu.sg>
2020-12-29 11:15:24 +0800

    Adjust some search results and page content

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var targetFood;
-
+var targetFoodKey;
  /*for youtube API
  ================================
 url = http://www.googleapis.com/youtube/v3/search?key=AIzaSyAX-5h3JcoZufXXPgVRiXK2en1OYGVVNGM&type=video&part=snippet&maxResults=10&q=chicken
@@ -13,13 +13,15 @@ var q= user_query;
 =============================================================
 */
 $("#Search-query").keyup(function(){
-    targetFood = document.getElementById("Search-query").value;
-})
-;
+
+    targetFood = $("#Search-query").val();
+    if (targetFood.includes(' ')){                         //finds query with spacing in words
+        targetFoodKey = targetFood.replaceAll(' ','+');// translate query to fit in the API request url
+    }                                                      // e.g. chicken rice --> chicken+rice
+});
 
 $(document).ready(function(){
-    
-       
+
     $("#Search-button").click(function(e){
        e.preventDefault();
        $("#result").before().text(`Search results for '${targetFood}'`);
@@ -30,24 +32,26 @@ $(document).ready(function(){
         $.ajax({
             dataType:'json',
             method:"GET",
-            url:`https://edamam-food-and-grocery-database.p.rapidapi.com/parser?ingr=${targetFood}`,
+            url:`https://edamam-food-and-grocery-database.p.rapidapi.com/parser?ingr=${targetFoodKey}`,
             "headers": {
                 "x-rapidapi-key": "e51e529e9emsh680c3ed1cdba0abp149d45jsn49661bfdcbeb",
                 "x-rapidapi-host": "edamam-food-and-grocery-database.p.rapidapi.com"
             }          
         })
         .done(function(data){
-            // $.each(data,function(key,value){
-            //     console.log(key+":" +value);
-            // });
             loadingCircle.fadeOut("slow");
             var nutrients  = data.parsed[0].food.nutrients;
             let ingrName = data.parsed[0].food.label;
             let ingrImage = data.parsed[0].food.image;
             $("#result").prepend("<div></div>"); //add a div element for each food ingredient or dish
-            $("#result div").css('background-color','red');
             $("#result div").append(`<h3>${ingrName}</h3>`);
-            $("#result div").append(`<img src = ${ingrImage} alt="Image not available" class ="img-thumbnail">`);
+            if (ingrImage === undefined){
+                $("#result div").append(`<span class ="img-thumbnail">Image not available</span><br>`);
+            }
+            else{
+                $("#result div").append(`<img src = ${ingrImage} alt="Image of ${ingrName}" class ="img-thumbnail"><br>`);
+            }
+
             $("#result div").append("<li></li>");
             for(x in nutrients){
                 nutrients[x] = nutrients[x].toFixed(2); //round up nutritional values to 2dp for simplicity
@@ -63,39 +67,45 @@ $(document).ready(function(){
                     $("#result div li").append(`<ul>${x} : ${nutrients[x]} </ul>`);
                 }            
             }
-            if(data.parsed[0].hasOwnProperty("foodContentsLabel")){
+            if(data.parsed[0].foodContentsLabel !== undefined){
                 $("#result").append(`Food contents: ${data.parsed[0].foodContentsLabel}`);
             };
             let ingrCount = 1; //ingrCount increments by 1 for every new instance of searched ingredient 
             for(let h = 0; h <data.hints.length;h++){
                 let similarIngr = data.hints[h].food.label;
                 let similarIngrImage = data.hints[h].food.image;
-
-                if ( similarIngr.includes(ingrName) || similarIngr.includes(ingrName.toUpperCase())){
+                let similarIngrContent = data.hints[h].foodContentsLabel;
+                if ( similarIngr.includes(targetFood) || similarIngr.includes(targetFood.toUpperCase())){  // retrieved query-related results
                     $("#result").append(`<div id = "other-ingr${ingrCount}"></div>`);
 
                     var childElement = `#result #other-ingr${ingrCount}`;  //childElement set to var to get updated upon ingrCount++
 
                     $(childElement).append(`<h3>${similarIngr}</h3>`);
                     var nutrients = data.hints[h].food.nutrients;
-                    $(childElement).append(`<img src = ${similarIngrImage} alt="Image not available" class ="img-thumbnail"><br>`);
+                    if (similarIngrImage === undefined){
+                        $(childElement).append(`<span class ="img-thumbnail">Image not available</span><br>`);
+                    }
+                    else{
+                        $(childElement).append(`<img src = ${similarIngrImage} alt="Image of ${similarIngr}" class ="img-thumbnail"><br>`);
+                    }
                     $(childElement).append("<li></li>");
 
                     for(x in nutrients){
                         nutrients[x] = nutrients[x].toFixed(2);     //round up nutritional values to 2dp for simplicity
                         if (x === "CHOCDF"){
                     
-                            $(`${childElement} li`).append(`<ul>carbohydrates : ${nutrients[x]}  </ul>`);
+                            $(`${childElement} li`).append(`<ul>carbohydrates : ${nutrients[x]}  </ul>`); //simplify 'CHOCDF' to 'Carbohydrates'
                         }
                         else if (x === "PROCNT"){
-                            $(`${childElement} li`).append(`<ul>Protein:${nutrients[x]} </ul>`);
+                            $(`${childElement} li`).append(`<ul>Protein:${nutrients[x]} </ul>`); //simplify 'PROCNT' to 'Protein'
                         }
                         else{
                             $(`${childElement} li`).append(`<ul>${x} : ${nutrients[x]} </ul>`);
                         }
-                    if(data.hints[h].hasOwnProperty("foodContentsLabel")){     //display foodContentsLabel for those ingredients or dishes with foodContentsLabel property
+                    if(similarIngrContent !== undefined){     //for ingredients & dishes with foodContentsLabel property
                         $(`${childElement}`).append(`Food contents: ${data.hints[h].foodContentsLabel}`);
                     }
+                    
                 }            
                 ingrCount++;
                 } 
